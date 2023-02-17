24af9b928400934c71e432554bdf68cc99bb4273
SaeedAlian <SaeedAlian.webdev@gmail.com>
2023-02-17 17:51:34 +0330

    setup tails
    -Added tails class
    --Included draw and update methods in tails class
    -Setup tail movements inside snake class

--- src/snake.py
+++ src/snake.py
@@ -9,13 +9,27 @@ class Snake:
     def __init__(self, surface):
         self.surface = surface
         self.direction = "RIGHT"
-        self.state = "MOVING"
+        self.state = "STOP"
         self.head = {
             "x": middleBoardCoordinate,
             "y": middleBoardCoordinate
         }
 
+        self.tails = []
+
+        self.addTail()
+
     def move(self):
+        self.moveHead()
+
+        for i in range(len(self.tails) - 1, -1, -1):
+            if i == 0:
+                self.tails[i].updateCoordinates(self.head["x"], self.head["y"])
+            else:
+                self.tails[i].updateCoordinates(
+                    self.tails[i - 1].x, self.tails[i - 1].y)
+
+    def moveHead(self):
         if self.state == "STOP":
             return
 
@@ -44,6 +58,10 @@ class Snake:
     def draw(self):
         self.drawRect(self.head['x'], self.head['y'])
         self.drawTriangle(self.head['x'], self.head['y'], self.direction)
+
+        for i in range(len(self.tails)):
+            if i != 0:
+                self.tails[i].draw()
 
     def drawRect(self, x, y):
         pygame.draw.rect(self.surface, CONSTANTS.SNAKE_COLOR, (
@@ -115,3 +133,22 @@ class Snake:
                 [secondVertexCoordinates["x"], secondVertexCoordinates["y"]]
             ]
         )
+
+    def addTail(self):
+        tail = Tail(self.surface, self.head["x"], self.head["y"])
+        self.tails.append(tail)
+
+
+class Tail:
+    def __init__(self, surface, x, y):
+        self.surface = surface
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        pygame.draw.rect(self.surface, CONSTANTS.SNAKE_COLOR,
+                         (self.x, self.y, CONSTANTS.PIXELS, CONSTANTS.PIXELS))
+
+    def updateCoordinates(self, x, y):
+        self.x = x
+        self.y = y
